78dc18f4e016f04c5aaf0ac8fff1c5be4335142d
Anjali Konda <kondaanjali64@gmailo.com>
2026-07-20 05:37:33 +0530

    Task 1 Portfolio Website Completed

--- script.js
+++ script.js
@@ -1,16 +1,110 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
+// ==========================
+// SMOOTH SCROLL
+// ==========================
 
-            const target = document.querySelector(this.getAttribute('href'));
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
 
-            if (target) {
-                target.scrollIntoView({
-                    behavior: "smooth",
-                    block: "start"
-                });
-            }
-        });
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if(target){
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
     });
+});
+
+
+// ==========================
+// ACTIVE NAVIGATION
+// ==========================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        if(pageYOffset >= sectionTop){
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === "#" + current){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
+
+
+// ==========================
+// SCROLL REVEAL
+// ==========================
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+document.querySelectorAll("section").forEach(sec=>{
+
+    sec.classList.add("hidden");
+
+    observer.observe(sec);
+
+});
+emailjs.init({
+  publicKey: "xODg86Y0MUlpr7dqk"
+});
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  try {
+    await emailjs.sendForm(
+      "service_lpvx5s5",
+      "template_jgnsi7l",
+      form
+    );
+
+    alert("Message sent successfully!");
+    form.reset();
+  } catch (err) {
+    console.log(JSON.stringify(err));
+    console.log(err);
+    alert(err.text || err.message || "Unknown error");
+}
 });
